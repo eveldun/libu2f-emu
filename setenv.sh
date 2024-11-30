9d5cb80ebbf73e8a30e53a46c957aaac20ac9be5
eveldun <eveldun123@protonmail.com>
2024-11-30 13:51:02 -0800

    added miniclean alias to setenv.sh development script

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,7 @@
 export CPATH=/usr/include/openssl-1.1
+export alias clean=git clean -dfx
 echo "CPATH Set: ${CPATH}"
 echo "LD_FLAGS Set ${LD_FLAGS}"
-export PROMPT_COMMAND='PS1_CMD1=$(pwd)'; 
+export PROMPT_COMMAND='PS1_CMD1=$(pwd)';
 export PS1='\[\e[38;5;208m\](\[\e[38;5;54m\]ENV\[\e[38;5;208m\])\[\e[38;5;51;1m\]${CPATH}\[\e[0;38;5;208m\]}\[\e[0m\]${PS1_CMD1}>'
 sh
